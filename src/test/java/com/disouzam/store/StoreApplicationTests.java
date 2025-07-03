f173b92579151dc0d19ce24dacb6301fbbb1604b
Dickson Souza <36424026+disouzam@users.noreply.github.com>
2025-07-02 22:11:21 -0300

    Projeto inicial conforme criado no start.spring.io (Spring Initializr)
    - git ignore foi mesclado com as configurações anteriores do GitHub

---
+++ src/test/java/com/disouzam/store/StoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.disouzam.store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
